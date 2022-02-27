1855c7f1dd7763e124d2c718c9edd85e41c20cca
naka-disc <99717730+naka-disc@users.noreply.github.com>
2022-02-27 12:00:25 +0000

    入力値を受け取って出力するまで

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+// 標準ライブラリ 入力受付など
+use std::io;
+
+// main関数がエントリーポイント
 fn main() {
-    println!("Hello, world!");
+  // println!で、画面上に出力
+  println!("Guess the number!");
+  println!("Please input your guess.");
+
+  // 変数定義 let mutなので、ミュータブル（変更可能）
+  let mut guess = String::new();
+
+  // read_lineは、ユーザーからの入力を受け付ける標準ライブラリの関数
+  io::stdin()
+    // 上で定義したguess変数に入力値を入れる
+    // &mut guess は、引数が3章であることを示し、mutに&をつけるのは、Rustがデフォルトでイミュータブルだから？
+    .read_line(&mut guess)
+    // パニック処理
+    // あんまりよくわからんここ
+    .expect("Failed to read line");
+
+  // 入力値をセットして出力
+  println!("You guessed: {}", guess);
 }
