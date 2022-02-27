7af44b90a135ebea907f5124b5b7ce19c9b9b922
naka-disc <99717730+naka-disc@users.noreply.github.com>
2022-02-27 14:28:29 +0000

    乱数と入力値がマッチするまでループするよう修正

--- src/main.rs
+++ src/main.rs
@@ -15,39 +15,46 @@ fn main() {
   let secret_number = rand::thread_rng().gen_range(1..101);
   println!("The secret number is: {}", secret_number);
 
-  // 入力を促す出力
-  println!("Please input your guess.");
-  // 変数定義 let mutなので、ミュータブル（変更可能）
-  let mut guess = String::new();
+  // 正解とマッチするまでループ
+  loop {
+    // 入力を促す出力
+    println!("Please input your guess.");
+    // 変数定義 let mutなので、ミュータブル（変更可能）
+    let mut guess = String::new();
 
-  // read_lineは、ユーザーからの入力を受け付ける標準ライブラリの関数
-  io::stdin()
-    // 上で定義したguess変数に入力値を入れる
-    // &mut guess は、引数が3章であることを示し、mutに&をつけるのは、Rustがデフォルトでイミュータブルだから？
-    .read_line(&mut guess)
-    // パニック処理
-    // あんまりよくわからんここ
-    .expect("Failed to read line");
+    // read_lineは、ユーザーからの入力を受け付ける標準ライブラリの関数
+    io::stdin()
+      // 上で定義したguess変数に入力値を入れる
+      // &mut guess は、引数が3章であることを示し、mutに&をつけるのは、Rustがデフォルトでイミュータブルだから？
+      .read_line(&mut guess)
+      // パニック処理
+      // あんまりよくわからんここ
+      .expect("Failed to read line");
 
-  // このままだと型が不一致なので、入力された値をパース
-  // u32は、符号なし32ビットの数値
-  let guess: u32 = guess
-    // 先頭と末尾のスペースを除去
-    .trim()
-    // パース。値に応じて、型推論してくれる
-    // @see https://doc.rust-lang.org/stable/std/primitive.str.html#method.parse
-    .parse()
-    // パニック処理
-    // あんまりよくわからんここ
-    .expect("Please type a number!");
+    // このままだと型が不一致なので、入力された値をパース
+    // u32は、符号なし32ビットの数値
+    let guess: u32 = guess
+      // 先頭と末尾のスペースを除去
+      .trim()
+      // パース。値に応じて、型推論してくれる
+      // @see https://doc.rust-lang.org/stable/std/primitive.str.html#method.parse
+      .parse()
+      // パニック処理
+      // あんまりよくわからんここ
+      .expect("Please type a number!");
 
-  // 乱数と変換後の入力値とを比較し、結果を出力
-  match guess.cmp(&secret_number) {
-    Ordering::Less => println!("Too small!"),
-    Ordering::Greater => println!("Too big!"),
-    Ordering::Equal => println!("You win!"),
+    // 乱数と変換後の入力値とを比較し、結果を出力
+    match guess.cmp(&secret_number) {
+      // guess < secret_number
+      Ordering::Less => println!("Too small!"),
+      // guess > secret_number
+      Ordering::Greater => println!("Too big!"),
+      // guess = secret_number
+      Ordering::Equal => {
+        println!("You win!");
+        break;
+      }
+    }
   }
 
-  // 入力値をセットして出力
-  println!("You guessed: {}", guess);
 }
